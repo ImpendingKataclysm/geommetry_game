8bcca3855da29f3ec6d128c65d4ac67f33f55f07
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-19 16:05:43 -0700

    Created a separate file for the data used to generate the rectangle so that the same values may be used by the command line version and the GUI version.

---
+++ coordinate_data.py
@@ -0,0 +1,5 @@
+# Random integer start and end parameters used to generate the rectangle:
+CORNER1_START = 0
+CORNER1_END = 400
+CORNER2_START = 10
+CORNER2_END = 400
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from random import randint
 from entities import Point, Rectangle
+import coordinate_data as cd
 
 if __name__ == "__main__":
-
-    low_min = 0
-    low_max = 9
-    up_min = 10
-    up_max = 19
-
-    low_left = Point(randint(low_min, low_max), randint(low_min, low_max))
-    up_right = Point(randint(up_min, up_max), randint(up_min, up_max))
+    low_left = Point(randint(cd.CORNER1_START, cd.CORNER1_END), randint(cd.CORNER1_START, cd.CORNER1_END))
+    up_right = Point(randint(cd.CORNER2_START, cd.CORNER2_END), randint(cd.CORNER2_START, cd.CORNER2_END))
 
     rect = Rectangle(low_left, up_right)
 
-    print(f"Rectangle Coordinates:\nBottom Left: ({rect.corner1.x}, {rect.corner1.y})\nTop Right: ({rect.corner2.x}, {rect.corner2.y})")
+    print(f"Rectangle Coordinates:\nCorner 1: ({rect.corner1.x}, {rect.corner1.y})\nCorner 2: ({rect.corner2.x}, {rect.corner2.y})")
     print("Guess a coordinate that falls within the rectangle!")
 
     user_x = float(input("X coordinate: "))
